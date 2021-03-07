33b51046d67edf575bbcabfc5f20dcea2757957b
j0ker <p-wegner.bremen@hotmail.com>
2021-03-07 18:35:44 +0100

    auskommentierung

--- UB4/py_ub4.py
+++ UB4/py_ub4.py
@@ -2,7 +2,7 @@
 def aufgabe1():
     zeichenkette = "[1,2,3,4, [a,b,c,d], help, run, [[a,b],[1,2]]]\n"
     print(zeichenkette)
-    neu_liste = zeichenkette.split()
+    #neu_liste = zeichenkette.split()
 
 def main():
     aufgabe1()
